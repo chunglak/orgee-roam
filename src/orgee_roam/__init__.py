bc93dc8d1afaff17b01f3e8174ef771893cd4393
Alfred LEUNG <alfred.leung@m4x.org>
2023-01-01 22:42:50 +0900

    New Zettel

--- src/orgee_roam/__init__.py
+++ src/orgee_roam/__init__.py
@@ -1,9 +1,12 @@
 # pylint: disable=unused-import
-from .zettel import Zettel
+from .zettel2 import Zettel
+
+# from .zettel import Zettel
 from .zettelkasten import ZettelKasten
-from .zk_func.make_zettel import make_zettel
-from .zk_func.list_zettel import make_list_zettel
-from .zk_func.finder_zettel import (
-    make_finder_files,
-    make_finder_files_by_creation_ts,
-)
+
+# from .zk_func.make_zettel import make_zettel
+# from .zk_func.list_zettel import make_list_zettel
+# from .zk_func.finder_zettel import (
+#     make_finder_files,
+#     make_finder_files_by_creation_ts,
+# )
